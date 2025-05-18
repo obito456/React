f9c0ab47a7886cdc6c79203d1979dfff8dc19683
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-05-18 14:59:30 +0530

    Create Color 01.jsx

---
+++ Color 01.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Test=()=>{
+    return(
+        <div>
+           <h1 style={{color:"blue"}}>hi</h1>
+           <h2 style={{color:"red"}}>hi</h2>
+           <h3 style={{color:"green"}}>hi</h3>
+           <h4 style={{color:"orange"}}>hi</h4>
+           <h5 style={{color:"grey"}}>hi</h5>
+           <h6 style={{color:"yellow"}}>hi</h6>
+        </div>
+    )
+}
+
+export default Test;
