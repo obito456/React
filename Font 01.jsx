870f8bbd3a23cad659966ba88af91b8f6b8c1a8a
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-05-18 16:23:28 +0530

    Create Font 01.jsx

---
+++ Font 01.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Test=()=>{
+    return(
+        <div>
+           <h1 style={{color:"blue"}}>hi</h1>
+           <p style={{color:"red",background:"white",fontSize:50}}> red </p>
+        </div>
+    )
+}
+
+export default Test;
